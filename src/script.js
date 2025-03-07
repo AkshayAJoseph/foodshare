edc37d3b2ad026650cfa4c9d2e98a87ecfac78c3
Sebastian <sebastianabraham2006@gmail.com>
2025-03-07 16:24:03 +0530

    feat: back button handling function for capacitor

---
+++ src/script.js
@@ -0,0 +1,22 @@
+import { Storage } from "@capacitor/storage";
+import { App } from "@capacitor/app";
+
+function handleBackButton(fallbackUrl) {
+    if (typeof window !== "undefined" && typeof sessionStorage !== "undefined") {
+      sessionStorage.setItem("fallbackPage", fallbackUrl);
+  
+      App.addListener("backButton", () => {
+        const prevPage = sessionStorage.getItem("fallbackPage");
+  
+        if (
+          window.location.href !== "https://localhost/" &&
+          window.location.href !== "https://localhost/home"
+        ) {
+          goto(prevPage, { replaceState: true });
+        } else {
+          App.exitApp();
+        }
+      });
+    } else {
+    }
+  }

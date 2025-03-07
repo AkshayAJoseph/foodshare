8d90a47b83e1fc89c52d547177eb8d995b9702b5
Sebastian <sebastianabraham2006@gmail.com>
2025-03-07 19:38:49 +0530

    feat: added create food functionality

--- goapi/controller/FoodFunctions.go
+++ goapi/controller/FoodFunctions.go
@@ -1,12 +1,35 @@
 package controller
 
 import (
-	"strings"
-
+	"github.com/foodshare/models"
 	"github.com/gofiber/fiber/v2"
 	"gorm.io/gorm"
 )
 
+func CreateFood(db *gorm.DB) func(*fiber.Ctx) error {
+	return func(c *fiber.Ctx) error {
+		food := new(models.Food)
 
+		err := c.BodyParser(food)
+		if err != nil {
+			return c.Status(400).JSON(fiber.Map{
+				"message": "Could not parse Body",
+				"error":   err.Error(),
+			})
+		}
 
+		err = db.Create(food).Error
+		if err != nil {
 
+			return c.Status(501).JSON(fiber.Map{
+				"message": "Could not create food",
+				"error":   err.Error(),
+			})
+		}
+
+		return c.Status(201).JSON(fiber.Map{
+			"message": "Recipe created",
+			"data":    food,
+		})
+	}
+}

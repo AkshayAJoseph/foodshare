ec3a52d2ba43d51fe5101893f2403195256b22df
Hari Narayan <harinarayanmr@aol.com>
2025-03-07 23:17:49 +0530

    feat: styled root, signup pages

--- src/routes/signup/+page.svelte
+++ src/routes/signup/+page.svelte
@@ -1,20 +1,65 @@
 <script>
-  import { handleBackButton, signup } from "../../script";
-  let email = $state("");
-  let password = $state("");
-  let name = $state("");
-  handleBackButton("/login");
-  const onclick = () => {
-    const data = { email, password, name };
-    signup(data);
-  };
+    import { handleBackButton, signup } from "../../script";
+    let email = $state("");
+    let password = $state("");
+    let name = $state("");
+    handleBackButton("/login");
+    const onclick = () => {
+        const data = { email, password, name };
+        signup(data);
+    };
+    import Header from "$lib/Header.svelte";
+    import Navigation from "$lib/Navigation.svelte";
+    import Progress from "$lib/Progress.svelte";
 </script>
 
-<h1>Login</h1>
-<label>email</label>
-<input type="text" placeholder="hari@laddu.cc" bind:value={email} />
-<label>password</label>
-<input type="password" bind:value={password} />
-<label>name</label>
-<input bind:value={name} />
-<button {onclick}>signup</button>
+<main>
+    <Progress order="1" />
+    <Header h1="Sign up" h5="Personal Details" />
+    <div class="box">
+        <div class="card">
+            <div class="card__title">
+                <h1>Please Fill</h1>
+            </div>
+            <div class="form">
+                <div class="form__row">
+                    <div class="form__row">
+                        <label>Are you an</label>
+                        <div class="flex">
+                            <div class="form__option">
+                                <h5>Individual</h5>
+                            </div>
+                            <div class="form__option">
+                                <h5>Organization</h5>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="form__row">
+                        <label>Name</label>
+                        <input bind:value={name} />
+                    </div>
+                    <div class="form__row">
+                        <label>Email</label>
+                        <input
+                            type="text"
+                            placeholder="hari@laddu.cc"
+                            bind:value={email}
+                        />
+                    </div>
+                    <div class="form__row">
+                        <label>Password</label>
+                        <input type="password" bind:value={password} />
+                    </div>
+                    <div class="btn--black">hey</div>
+                </div>
+            </div>
+        </div>
+    </div>
+</main>
+
+<style>
+    main {
+        background: var(--color-primary);
+        min-height: 100vh;
+    }
+</style>

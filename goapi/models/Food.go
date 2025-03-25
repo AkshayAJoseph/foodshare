3c61042b5bb43aa7b0421f34ded9aa003b3aae8d
Sebastian <sebastianabraham2006@gmail.com>
2025-03-25 21:23:18 +0530

    feat: image attribute for food

--- goapi/models/Food.go
+++ goapi/models/Food.go
@@ -5,14 +5,15 @@ import (
 )
 
 type Food struct {
-	FoodID      uint    `gorm:"primaryKey;autoIncrement" json:"id"`
-	Name        *string `gorm:"type:varchar(255)" json:"name"`
-	Lifespan    int     `gorm:"type:int" json:"lifespan"`
-	Quantity    int     `gorm:"type:int" json:"quantity"`
-	Longitutde  float64 `gorm:"type:float" json:"longitude"`
-	Latitude    float64 `gorm:"type:float" json:"latitude"`
-	Category    string  `gorm:"type:varchar(255)" json:"category"`
-	Tags        string  `gorm:"type:varchar(255)" json:"tags"`
+	FoodID     uint    `gorm:"primaryKey;autoIncrement" json:"id"`
+	Banner     string  `gorm:"type:varchar(255)" json:"banner"`
+	Name       *string `gorm:"type:varchar(255)" json:"name"`
+	Lifespan   int     `gorm:"type:int" json:"lifespan"`
+	Quantity   int     `gorm:"type:int" json:"quantity"`
+	Longitutde float64 `gorm:"type:float" json:"longitude"`
+	Latitude   float64 `gorm:"type:float" json:"latitude"`
+	Category   string  `gorm:"type:varchar(255)" json:"category"`
+	Tags       string  `gorm:"type:varchar(255)" json:"tags"`
 }
 
 func MigrateFood(db *gorm.DB) error {
